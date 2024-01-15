43c74b5fc934a4b02a333e19eed14d139c4757b6
exaul <137360873+exaul@users.noreply.github.com>
2024-01-15 00:10:09 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/EstudiantesController.cs
@@ -0,0 +1,40 @@
+﻿using APIESTUDIANTES.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIESTUDIANTES.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudiantesController : Controller
+    {
+        private static List<Estudiante> _estudiantes = new List<Estudiante>
+        {
+            new Estudiante
+            {
+                Id = 1,
+                Nombre ="Josua",
+                Curso ="9No 'E'"
+            },
+             new Estudiante
+            {
+                Id = 2,
+                Nombre ="Gustavo",
+                Curso ="1ero bachillerato 'X'"
+            },
+              new Estudiante
+            {
+                Id = 3,
+                Nombre ="Samuel",
+                Curso ="2do bachillerato 'A'"
+            },
+        };
+
+        [HttpGet]
+
+        public IEnumerable<Estudiante> GetAllEstudiantes()
+        {
+            return _estudiantes;
+
+        }
+    }
+}
---
+++ Models/Estudiante.cs
@@ -0,0 +1,11 @@
+﻿namespace APIESTUDIANTES.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Curso { get; set; }
+    }
+}
